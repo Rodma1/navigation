eaa379f264be1d38b73a520468fdbe609906a509
“Rodma1” <1156028692@qq.com>
2024-07-15 18:05:37 +0800

    feat: 测试类修改

--- src/test/java/ElasticsearchTest.java
+++ src/test/java/ElasticsearchTest.java
@@ -1,5 +1,7 @@
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch.core.InfoResponse;
+import co.elastic.clients.elasticsearch.indices.PutAliasRequest;
+import co.elastic.clients.elasticsearch.indices.PutAliasResponse;
 import com.chen.ChenApplication;
 import com.chen.common.config.elasticsearch.ElasticsearchClientConfig;
 import com.chen.domain.elsaticsearch.ElasticsearchConnectParam;
@@ -11,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: 神的孩子都在歌唱
@@ -33,6 +37,12 @@ public class ElasticsearchTest {
                 new ElasticsearchConnectParam("cosa_manage", "Y8jpam323Ptm^QQk%QxUkZoZ", "192.168.1.83", 9200, "http"));
         ElasticsearchClient elasticsearchClient = elasticsearchClientConfig.elasticsearchClient();
 
+        List<String> strings = new ArrayList<>();
+        strings.add("pppp_1");
+        strings.add("pppp");
+        PutAliasRequest build = new PutAliasRequest.Builder().index(strings).name("pppp_alias").isWriteIndex(true).build();
+
+        PutAliasResponse putAliasResponse = elasticsearchClient.indices().putAlias(build);
         long count = elasticsearchClient.count().count();
         System.out.println("资本三个月");
         System.out.println(count);

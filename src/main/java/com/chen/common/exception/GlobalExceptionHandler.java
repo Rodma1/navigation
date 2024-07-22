f5e0c3b0cdbe58107d1f1ac198b3757a1fb6161c
“Rodma1” <1156028692@qq.com>
2024-07-22 16:02:06 +0800

    feat: 全局异常处理

---
+++ src/main/java/com/chen/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.chen.common.exception;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.chen.common.utils.StringUtils;
+import com.chen.common.utils.resultreturn.ResultData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author chenyunzhi
+ * @DATE 2024/7/22 15:40
+ * @Description: 全局异常处理
+ */
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+
+    /**
+     * 业务异常
+     */
+    @ExceptionHandler(ServiceException.class)
+    public ResultData<Object> handleServiceException(ServiceException e, HttpServletRequest request) {
+        log.error(e.getMessage(), e);
+        Integer code = e.getCode();
+        return StringUtils.isNotNull(code) ? ResultData.error(code, e.getMessage()) : ResultData.error(e.getMessage());
+    }
+
+    /**
+     * 系统异常
+     */
+    @ExceptionHandler(Exception.class)
+    public ResultData<Object> handleException(Exception e, HttpServletRequest request) {
+        String requestUrl = request.getRequestURI();
+        log.error("请求地址'{}',发生系统异常.", requestUrl, e);
+        return ResultData.error(String.format("请求地址'%s',发生未知异常.", requestUrl));
+    }
+
+}

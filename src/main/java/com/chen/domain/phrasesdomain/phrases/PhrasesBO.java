52770edfc3da33de2655944f4def13ba5d6044c3
“Rodma1” <1156028692@qq.com>
2024-08-13 14:28:19 +0800

    feat: 句子增删改查修改

--- src/main/java/com/chen/domain/phrasesdomain/phrases/PhrasesBO.java
+++ src/main/java/com/chen/domain/phrasesdomain/phrases/PhrasesBO.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 
 import com.chen.common.config.mybatisplus.core.command.BaseBizCommand;
 import com.chen.common.exception.ServiceException;
@@ -57,6 +58,10 @@ public class PhrasesBO implements Serializable, BaseBizCommand<PhrasesPO> {
 
     @ApiModelProperty("父类")
     private String parentClass;
+
+
+    @ApiModelProperty("文章类别ids")
+    private List<Long> categoryIds;
 
     private Long id;
 
--- src/main/java/com/chen/service/phrasesservice/phrases/impl/PhrasesServiceImpl.java
+++ src/main/java/com/chen/service/phrasesservice/phrases/impl/PhrasesServiceImpl.java
@@ -8,20 +8,28 @@ import com.chen.common.config.mybatisplus.page.PageRequest;
 import com.chen.common.exception.ServiceException;
 import com.chen.common.utils.StringUtils;
 import com.chen.common.utils.page.PageUtils;
+import com.chen.domain.phrasesdomain.phrasesbindcategory.PhrasesBindCategoryPO;
+import com.chen.domain.phrasesdomain.phrasesbindcategory.PhrasesBindCategoryPO;
+import com.chen.domain.phrasesdomain.phrasesbindcategory.PhrasesBindCategoryBO;
+import com.chen.domain.phrasesdomain.phrasesbindcategory.PhrasesBindCategoryPO;
 import com.chen.domain.phrasesdomain.phrases.PhrasesBO;
 import com.chen.domain.phrasesdomain.phrases.PhrasesDTO;
 import com.chen.domain.phrasesdomain.phrases.PhrasesPO;
 import com.chen.domain.phrasesdomain.phrases.PhrasesPagesQuery;
 import com.chen.domain.phrasesdomain.phrasesCategory.PhrasesCategoryPO;
+import com.chen.domain.phrasesdomain.phrasesbindcategory.PhrasesBindCategoryBO;
 import com.chen.domain.phrasesdomain.phrasesbindcategory.PhrasesBindCategoryPO;
 import com.chen.mapper.PhrasesCategoryMapper;
 import com.chen.mapper.PhrasesMapper;
+import com.chen.service.phrasesservice.phrasesbindcategory.PhrasesBindCategoryService;
 import com.github.yulichang.wrapper.MPJLambdaWrapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.chen.common.config.mybatisplus.core.ServicePlusImpl;
 import com.chen.service.phrasesservice.phrases.PhrasesService;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -36,6 +44,7 @@ import java.util.stream.Collectors;
 public class PhrasesServiceImpl extends ServicePlusImpl<PhrasesMapper, PhrasesPO, PhrasesDTO> implements PhrasesService {
 
     private final PhrasesCategoryMapper categoryMapper;
+    private final PhrasesBindCategoryService phrasesBindCategoryService;
     @Override
     public TableDataInfo<PhrasesDTO> page(PhrasesPagesQuery pagesQuery) {
         MPJLambdaWrapper<PhrasesDTO> queryWrapper = new MPJLambdaWrapper<>();
@@ -46,34 +55,48 @@ public class PhrasesServiceImpl extends ServicePlusImpl<PhrasesMapper, PhrasesPO
 
         queryWrapper.orderByDesc(PhrasesDTO:: getCreateTime);
         PagePlus<PhrasesPO, PhrasesDTO>  pagedBo = this.pageBo(PageUtils.buildPagePlus(new PageRequest.Builder(pagesQuery.getPageNum(), pagesQuery.getPageSize()).build()));
-        this.parameterConversion(pagedBo.getRecordsVo());
+        pagedBo.getRecordsVo().forEach(this::parameterConversion);
         return PageUtils.buildDataInfo(pagedBo);
     }
 
     /**
      * 转化特定参数
-     * @param articleDtoS 文章
+     * @param phrasesDtoS 文章
      */
-    private void parameterConversion(List<PhrasesDTO> articleDtoS) {
+    private void parameterConversion(PhrasesDTO phrasesDtoS) {
         Map<Long, String> collect = categoryMapper.selectList(new LambdaQueryWrapper<>()).stream().collect(Collectors.toMap(PhrasesCategoryPO::getId, PhrasesCategoryPO::getName));
-        articleDtoS.forEach(item->{
-            item.setCategoryName(collect.get(item.getCategoryId()));
-        });
+        List<PhrasesBindCategoryBO> phrasesBindCategoryBoS = phrasesBindCategoryService.listBo(new LambdaQueryWrapper<PhrasesBindCategoryPO>().eq(PhrasesBindCategoryPO::getPhrasesId, phrasesDtoS.getId()));
+
+        phrasesDtoS.setCategoryName(phrasesBindCategoryBoS.stream().map(item -> collect.get(item.getCategoryId())).collect(Collectors.joining(", ")));
     }
+    @Transactional(rollbackFor = ServiceException.class)
     @Override
     public Boolean insert(PhrasesBO phrasesBO) {
-        boolean save = this.save(phrasesBO.buildInsertPo());
-        if (!save) {
-            throw new ServiceException("新增失败");
+        try {
+            PhrasesPO phrasesPo = phrasesBO.buildInsertPo();
+            boolean save = this.save(phrasesPo);
+            if (!save) {
+                throw new ServiceException("新增失败");
+            }
+            this.phrasesBindCategory(phrasesBO.getCategoryIds(), phrasesPo.getId());
+            return true;
+        } catch (Exception e) {
+            throw new ServiceException("新增失败" + e.getMessage());
         }
-        return true;
     }
 
+    @Transactional(rollbackFor = ServiceException.class)
     @Override
     public Boolean delete(Long id) {
-        boolean remove = this.removeById(id);
-        if (!remove) {
-            throw new ServiceException("删除失败");
+
+        try {
+            boolean remove = this.removeById(id);
+            if (!remove) {
+                throw new ServiceException("删除失败");
+            }
+            phrasesBindCategoryService.remove(new LambdaQueryWrapper<PhrasesBindCategoryPO>().eq(PhrasesBindCategoryPO::getPhrasesId,id));
+        } catch (ServiceException e) {
+            throw new ServiceException(e);
         }
         return true;
 
@@ -81,11 +104,31 @@ public class PhrasesServiceImpl extends ServicePlusImpl<PhrasesMapper, PhrasesPO
 
     @Override
     public Boolean update(PhrasesBO phrasesBO) {
-        boolean update = this.save(phrasesBO.buildUpdatePo());
-        if (!update) {
-            throw new ServiceException("更新失败");
+        try {
+            boolean update = this.save(phrasesBO.buildUpdatePo());
+            if (!update) {
+                throw new ServiceException("更新失败");
+            }
+            phrasesBindCategoryService.remove(new LambdaQueryWrapper<PhrasesBindCategoryPO>().eq(PhrasesBindCategoryPO::getPhrasesId,phrasesBO.getId()));
+            this.phrasesBindCategory(phrasesBO.getCategoryIds(), phrasesBO.getId());
+        } catch (ServiceException e) {
+            throw new ServiceException(e);
         }
         return true;
 
     }
+
+    private void phrasesBindCategory(List<Long> categoryIds,Long phrasesId) {
+        ArrayList<PhrasesBindCategoryPO> phrasesBindCategoryPoS = new ArrayList<>();
+
+        categoryIds.forEach(item-> {
+            PhrasesBindCategoryPO phrasesBindCategoryPo = new PhrasesBindCategoryPO();
+            phrasesBindCategoryPo.setCategoryId(item);
+            phrasesBindCategoryPo.setPhrasesId(phrasesId);
+            phrasesBindCategoryPoS.add(phrasesBindCategoryPo);
+
+        });
+        phrasesBindCategoryService.saveBatch(phrasesBindCategoryPoS);
+    }
+
 }
